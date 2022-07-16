e52c198ce8889e6ac2b4e6b080bbd0f943c35732
Musa Kazim <musakazim989@gmail.com>
2022-07-16 16:04:07 +0600

    update operated successfully

--- backend/routes/serverRoute.js
+++ backend/routes/serverRoute.js
@@ -2,6 +2,13 @@ import express from "express"
 import serverMode from "../model/serverMode.js"
 const serverRouter = express.Router()
 
+serverRouter.post("/delete", async (req, res) => {
+  serverMode.findByIdAndDelete(req.body.id, (err, docs) => {
+    err ? console.log(err) : `Delete: ${docs}`
+  })
+  // console.log(req.body.id)
+})
+
 serverRouter.post("/add", (req, res) => {
   const details = {
     task: req.body.users.task,
@@ -19,11 +26,21 @@ serverRouter.get("/get", async (req, res) => {
   res.send(tododetails)
 })
 
-serverRouter.post("/delete", async (req, res) => {
-  serverMode.findByIdAndDelete(req.body.id, (err, docs) => {
-    err ? console.log(err) : `Delete: ${docs}`
-  })
-  // console.log(req.body.id)
+serverRouter.put("/edit", (req, res) => {
+  let updatedetails = {
+    task: req.body.updateinfo.task,
+    durationtime: req.body.updateinfo.durationTime,
+    date: req.body.updateinfo.date,
+  }
+  serverMode.findByIdAndUpdate(
+    req.body.updateinfo.id,
+    updatedetails,
+    (err, docs) => {
+      err ? console.log(err) : `Updated: ${docs}`
+    }
+  )
+
+  console.log(req.body.updateinfo.id)
 })
 
 export default serverRouter
