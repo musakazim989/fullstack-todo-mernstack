180db71e0b219e060aa15160121f0d88a4beab6c
Musa Kazim <musakazim989@gmail.com>
2022-07-16 12:36:25 +0600

    delete item added

--- backend/routes/serverRoute.js
+++ backend/routes/serverRoute.js
@@ -6,6 +6,7 @@ serverRouter.post("/add", (req, res) => {
   const details = {
     task: req.body.users.task,
     durationtime: req.body.users.durationTime,
+    date: req.body.users.date,
   }
 
   const tododetails = new serverMode(details)
@@ -16,6 +17,13 @@ serverRouter.post("/add", (req, res) => {
 serverRouter.get("/get", async (req, res) => {
   const tododetails = await serverMode.find()
   res.send(tododetails)
+})
+
+serverRouter.post("/delete", async (req, res) => {
+  serverMode.findByIdAndDelete(req.body.id, (err, docs) => {
+    err ? console.log(err) : `Delete: ${docs}`
+  })
+  // console.log(req.body.id)
 })
 
 export default serverRouter
